807868f1d200d746ce276121b24dafbbd53faf79
awakcon1234 <150042917+awakcon1234@users.noreply.github.com>
2025-02-15 10:17:47 +0700

    change nova version to `0.18-alpha.13`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.18-alpha.19")
+            from("xyz.xenondevs.nova:catalog:0.18-alpha.13")
         }
     }
 }
